195fd602677814640bc88b66484cc984a3b6a5bd
Rodrigo Costa <svork@users.noreply.github.com>
2018-03-18 13:29:46 -0300

    Link para Facebook

---
+++ javascript/agendar.js
@@ -0,0 +1,4 @@
+// Esta função conecta ao Facebook do Cliente
+function conectar_facebook(){
+  alert("Ainda não está pronto, aguarde...");
+}
---
+++ javascript/sorteio.js
@@ -0,0 +1,6 @@
+// Este arquivo é apenas uma função teste em JavaScript
+function estamos_desenvolvendo(){
+  document.getElementById("teste01").innerHTML = "Esta página está sendo desenvolvida, volte mais tarde";
+  alert("Ainda em testes, aguarde!");
+  console.log("Mensagem de teste na console do navegador, botão F12");
+}
